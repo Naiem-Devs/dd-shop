73acb421c475f1606cb71395a42e9c97faab3494
Naiem-Devs <dnsnaiem@gmail.com.com>
2024-10-05 01:19:52 +0600

    all done

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,38 +9,18 @@
   }); 
 
 
-  // owlCarousel
-  $(".brand-active").owlCarousel({
-    loop: true,
-    margin: 30,
-    items: 6,
-    navText: [
-      '<i class="fa fa-angle-left"></i>',
-      '<i class="fa fa-angle-right"></i>'
-    ],
-    nav: false,
-    dots: false,
-    responsive: {
-      0: {
-        items: 2
-      },
-      767: {
-        items: 3
-      },
-      992: {
-        items: 6
-      }
-    }
+  jQuery('.scroll').onePgaeNav({
+    wrapper: '#onepage-nav',
+    activeClass: 'active'
   });
 
-
   // page Animation
-  // AOS.init({
-  //   mirror: true,
-  //   duration: 1500,
-  //   initClassName: 'aos-init',
-  //   once: true,
-  // });
+  AOS.init({
+    mirror: true,
+    duration: 1500,
+    initClassName: 'aos-init',
+    once: true,
+  });
 
   // data-aos="fade-up" 
   // data-aos-delay="300" 
